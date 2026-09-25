8ff59e4501bc43068b61734590f6464eac589598
SauceCode01 <sauce.code.01@gmail.com>
2026-09-25 20:13:29 +0800

    feat: fixed pnpm ignored build error during tests

--- backend/src/pxExpress/index.ts
+++ backend/src/pxExpress/index.ts
@@ -1,6 +1,5 @@
-import { DeviceIdentityMiddleware } from "./middlewares/DeviceIdentityMiddleware.js";
 import { GlobalErrorHandler } from "./middlewares/GlobalErrorHandler.js";
 import { LoggingMiddleware } from "./middlewares/LoggingMiddleware.js";
 
-export const middlewares = [LoggingMiddleware, DeviceIdentityMiddleware];
+export const middlewares = [LoggingMiddleware];
 export const errorHandlers = [GlobalErrorHandler];
